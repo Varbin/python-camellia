02f8371d096cf9702f5292369e28917c2d49f0da
Varbin <simon.biewald@hotmail.de>
2015-08-29 22:21:59 +0200

    Added hint for after-install command, added __main__.py to provide camellia commandline script.

---
+++ camellia/__main__.py
@@ -0,0 +1,6 @@
+import camellia
+
+if camellia.test() == 'passed':
+    print("Camellia is installed on your computer!")
+else:
+    print("There is an error in your configuration! Please report at github!")
