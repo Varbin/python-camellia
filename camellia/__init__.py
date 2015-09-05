90b477b9252d45aae5b53de8b46f49b18a982793
Varbin <simon.biewald@hotmail.de>
2015-09-05 12:17:34 +0200

    Cleaned up code

--- camellia/__init__.py
+++ camellia/__init__.py
@@ -1,11 +1,9 @@
-try:
-    __file__
-except NameError:
-    __file__ == "."
+#!/usr/bin/python3
 
-from ctypes import *
+from ctypes import create_string_buffer, CDLL
 import os
-
+import sys
+import platform
 
 MODE_ECB = 1
 MODE_CBC = 2
@@ -13,15 +11,12 @@ MODE_CFB = 3
 MODE_OFB = 5
 MODE_CTR = 6
 
-import os
-import sys
-import platform
 
 d = dict(
     linux='.so',
     freebsd='.so',
     openbsd='.so',
-    unix='.so', #???
+    unix='.so',  # ???
     darwin='.so',
     win32='.dll',
     cygwin='.dll',
@@ -30,15 +25,26 @@ d = dict(
 
 d['Pocket PC'] = '.dll'
 
+try:
+    __file__
+except NameError:
+    __file__ == "."
+
+
+class camelliaException(Exception):
+    pass
+
+
 def version_string():
-    plat = sys.platform.replace('linux2','linux').replace('linux3', 'linux')
-    
+    plat = sys.platform.replace('linux2', 'linux').replace('linux3', 'linux')
+
     base = "camellia-{}-{}-{}"+d.get(plat, '.???')
 
     proc = platform.machine() if platform.machine else "unknown"
     arch = platform.architecture()[1]
 
     return base.format(plat, proc, arch)
+
 
 ADD = "./" if not os.path.dirname(__file__) else ""
 
@@ -48,8 +54,6 @@ OUT = os.path.join(os.path.dirname(__file__), version_string())
 GCC = os.environ.get('CC', 'gcc')
 CMD = "%s %s -shared -fPIC -O3 -o%s" % (GCC, IN, OUT)
 
-class camelliaException(Exception):
-    pass
 
 if not os.path.exists(OUT):
     print("Compiling camellia with %s..." % GCC)
@@ -57,13 +61,18 @@ if not os.path.exists(OUT):
     try:
         assert not os.system(CMD)
     except AssertionError:
-        print ("Please install gcc and include camellia.c with this file, then run with sudo to compile!")
+        print("Please install gcc and include camellia.c with this file, "
+              "then run with sudo to compile!")
+
 
 try:
     camlib = CDLL(ADD+OUT)
 except:
-    print ("Please install gcc and include camellia.c with this file, then run with sudo to compile!")
-    raise camelliaException("camellia.shared not found. Please install gcc and include camellia.c with this file, then run with sudo to compile!")
+    print("Please install gcc and include camellia.c with this file, "
+          "then run with sudo to compile!")
+    raise camelliaException(version_string()+" not found. Please install gcc "
+                            "and include camellia.c with this file, then run "
+                            "with sudo to compile!")
 
 
 def Camellia_Ekeygen(rawKey):
@@ -71,7 +80,7 @@ def Camellia_Ekeygen(rawKey):
 
     keytable = create_string_buffer(272)  # create buffer to write in
     camlib.Camellia_Ekeygen(len(rawKey)*8, rawKey, keytable)
-    
+
     return keytable.raw  # return as bytes
 
 
@@ -95,17 +104,16 @@ def Camellia_Decrypt(keyLength, keytable, cipherText):
     return clear.raw
 
 
-
 class CamelliaCipher(object):
     block_size = 16*8
-    
+
     def __init__(self, key, **kwargs):
         self.__key_length = len(key) * 8
         self.__key = Camellia_Ekeygen(key)
-        
+
         if len(key) not in [16, 24, 32]:
             raise ValueError("Key must be 128, 192 or 256 bits long")
-        
+
         keys = kwargs.keys()
         if "mode" in keys:
             self.mode = kwargs["mode"]
@@ -113,7 +121,7 @@ class CamelliaCipher(object):
                 raise NotImplementedError("This mode is not supported!")
         else:
             self.mode = MODE_ECB
-            
+
         if "IV" in keys:
             self.__IV = kwargs["IV"]
             if len(self.__IV) != self.block_size/8:
@@ -124,28 +132,31 @@ class CamelliaCipher(object):
         elif self.mode == MODE_CTR:
             raise ValueError("CTR needs a counter!")
 
-
     def encrypt(self, data):
         blocks = self._block(data)
-        
+
         if self.mode == MODE_ECB:
             if len(data) % (self.block_size/8):
-                raise ValueError("Input string must be a multiple of blocksize in length")
+                raise ValueError("Input string must be a multiple "
+                                 "of blocksize in length")
 
             out = []
             for block in blocks:
-                out.append(Camellia_Encrypt(self.__key_length, self.__key, block))
+                out.append(Camellia_Encrypt(self.__key_length, self.__key,
+                                            block))
 
             return b''.join(out)
 
         elif self.mode == MODE_CBC:
             if len(data) % (self.block_size/8):
-                raise ValueError("Input string must be a multiple of blocksize in length")
+                raise ValueError("Input string must be a multiple "
+                                 "of blocksize in length")
 
             out = []
             for block in blocks:
                 xored = xor(block, self.__IV)
-                self.__IV = (Camellia_Encrypt(self.__key_length, self.__key, xored))
+                self.__IV = (Camellia_Encrypt(self.__key_length, self.__key,
+                                              xored))
 
                 out.append(self.__IV)
 
@@ -154,32 +165,35 @@ class CamelliaCipher(object):
         else:
             raise Exception("???")
 
-
     def decrypt(self, data):
         blocks = self._block(data)
-        
+
         if self.mode == MODE_ECB:
             if len(data) % (self.block_size/8):
-                raise ValueError("Input string must be a multiple of blocksize in length")
+                raise ValueError("Input string must be a multiple "
+                                 "of blocksize in length")
 
             out = []
             for block in blocks:
-                out.append(Camellia_Decrypt(self.__key_length, self.__key, block))
+                out.append(Camellia_Decrypt(self.__key_length, self.__key,
+                                            block))
 
             return b''.join(out)
 
         elif self.mode == MODE_CBC:
             if len(data) % (self.block_size/8):
-                raise ValueError("Input string must be a multiple of blocksize in length")
+                raise ValueError("Input string must be a multiple "
+                                 "of blocksize in length")
 
             out = []
             blocks = [self.__IV] + blocks
             for i in range(1, len(blocks)):
-                temp = Camellia_Decrypt(self.__key_length, self.__key, blocks[i])
+                temp = Camellia_Decrypt(self.__key_length, self.__key,
+                                        blocks[i])
                 out.append(xor(temp, blocks[i-1]))
 
             self.__IV = blocks[-1]
-            
+
             return b''.join(out)
 
         else:
@@ -191,22 +205,20 @@ class CamelliaCipher(object):
         for i in range(int(len(s)/(self.block_size/8))):
             l.append(s[i*(self.block_size//8):((i+1)*(self.block_size//8))])
         if rest_size:
-            #raise ValueError()
+            # raise ValueError()
             l.append(s[-rest_size:])
         return l
 
 
-import sys
-
-def xor(a,b):
+def xor(a, b):
     if int(sys.version[0]) < 3:
-        return "".join([chr(ord(c)^ord(d)) for c,d in zip(a,b)])
-    return bytes([c^d for c,d in zip(a,b)])
+        return "".join([chr(ord(c) ^ ord(d)) for c, d in zip(a, b)])
+    return bytes([c ^ d for c, d in zip(a, b)])
 
 
 def test():
-    key    = b"80000000000000000000000000000000"
-    plain  = b"00000000000000000000000000000000"
+    key = b"80000000000000000000000000000000"
+    plain = b"00000000000000000000000000000000"
     cipher = b"6C227F749319A3AA7DA235A9BBA05A2C"
 
     import binascii
@@ -228,9 +240,8 @@ def test():
         print("Result:\t\tcipher=%s" % binascii.hexlify(dc).decode())
         print("Required:\tcipher=%s" % plain.decode())
         return "failed"
-    
 
     return "passed"
 
 if __name__ == "__main__":
-    print ("Test: "+test())
+    print("Test: "+test())
