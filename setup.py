6b32262db5641711d4a8d7b14f878430941e690e
Simon Biewald <simon@fam-biewald.de>
2020-09-24 18:16:54 +0200

    Change build mode of CFFI extension
    It is recommended to use cffi_modules, this also enables
    building with ABI3 flags.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from __future__ import print_function
 
 import sys
 import os
-import glob
+
 
 try:
     from setuptools import setup, find_packages
@@ -24,11 +24,8 @@ except ImportError:
 
     sys.exit(1)
 
-sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
 
-import camellia_build  # nopep8
-
-description = 'Camellia-cipher in Python'
+description = 'Camellia block cipher in Python'
 
 
 def long_description(short=description):
@@ -39,31 +36,31 @@ def long_description(short=description):
         return short
 
 
-ext = camellia_build.ffi.distutils_extension()
-ext.include_dirs.append(
-    os.path.join(os.path.dirname(__file__), 'src', 'camellia_build'))
-
-
 setup(
     name='python-camellia',
     version='1.0',
     description=description,
     long_description=long_description(),
     author='Simon Biewald',
-    author_email='simon.biewald@homtail.de',
-    url='https://github.com/var-sec/python-camellia',
+    author_email='simon@fam-biewald.de',
+    url='https://github.com/varbin/python-camellia',
     project_urls={
-        'Documentation': 'https://sbiewald.de/docs/python-camellia',
+        'Documentation': 'https://python-camellia.readthedocs.io',
+        'Source': 'https://github.com/Varbin/python-camellia',
         'Tracker': 'https://github.com/Varbin/python-camellia/issues'
     },
     packages=['camellia'],
     package_dir={'camellia': 'src/camellia'},
 
-    license='MIT',
+    cffi_modules=['src/_camellia_build/camellia_build.py:ffi'],
+
+    license='MIT and BSD-2-Clause',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: C',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
@@ -73,12 +70,11 @@ setup(
         'Topic :: Security :: Cryptography',
     ],
     keywords=[
-        'camellia', 'encryption', 'decryption', 'cipher',
+        'camellia', 'encryption', 'block cipher'
     ],
 
     platforms=['all'],
 
-    ext_modules=[ext],
     setup_requires=['cffi>=1.0.0'],
     install_requires=['cffi>=1.0.0', 'pep272-encryption']
 
---
+++ src/_camellia_build/camellia_build.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Builder for the C-extension of python-camellia.
+"""
+
+from distutils.ccompiler import new_compiler
+
+from cffi import FFI
+from setuptools import Distribution
+
+import os
+
+HERE = os.path.dirname(__file__)
+
+SOURCE_FILE = "camellia.c"
+HEADER_FILE = "camellia.h"
+
+ABSOLUTE_SOURCE_FILE = os.path.join(HERE, SOURCE_FILE)
+ABSOLUTE_HEADER_FILE = os.path.join(HERE, HEADER_FILE)
+
+with open(ABSOLUTE_HEADER_FILE) as f:
+    header = f.read()
+
+with open(ABSOLUTE_SOURCE_FILE) as f:
+    source = f.read()
+
+
+def extra_link_args():
+    dist = Distribution()
+    dist.parse_config_files()
+    dist.parse_command_line()
+    build = dist.get_command_obj('build')
+    build.ensure_finalized()
+    if new_compiler(compiler=build.compiler).compiler_type == 'msvc':
+        return ["/NXCOMPAT", "/DYNAMICBASE"]
+    else:
+        return []
+
+
+ffi = FFI()
+ffi.cdef(header)
+ffi.set_source("camellia._camellia", source,
+               include_dirs=[HERE], extra_link_args=extra_link_args())
